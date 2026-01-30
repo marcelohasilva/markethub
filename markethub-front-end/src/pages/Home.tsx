0162a8892ae0c33d9636ccbbf34320acad97beb2
ricardo-nascimento-cloud <ricardonascimento1506@gmail.com>
2026-01-30 19:41:59 -0300

    feat: adicionei card de produto na home

--- markethub-front-end/src/pages/Home.tsx
+++ markethub-front-end/src/pages/Home.tsx
@@ -2,17 +2,16 @@ import CardProduto from "../components/CardProduto";
 import HeaderMain from "../components/HeaderMain";
 
 const Home = () => {
-  return (
-    <>
-      <HeaderMain />
-
-      <h1 className="text-[#282729] mt-5 text-3xl ml-24 font-bold">
-        Destaques
-      </h1>
-
-      <CardProduto />
-    </>
-  );
-};
+    return(
+        <>
+         <HeaderMain />
+         <h1 className="text-[#282729]  mt-5 text-3xl ml-24 font-bold">
+            Destaques
+         </h1>
+        <CardProduto />
+        </>
+       
+    )
+}
 
 export default Home;
