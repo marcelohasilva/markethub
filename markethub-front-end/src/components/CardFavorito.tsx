3bf39fbdf511ee01730bf8f79dd9e996c13883cb
Marcelo Henrique Almeida da Silva <marcelo.hasilva06@gmail.com>
2026-02-07 18:40:08 -0300

    feat: tela de favoritos

--- markethub-front-end/src/components/CardFavorito.tsx
+++ markethub-front-end/src/components/CardFavorito.tsx
@@ -1,20 +1,28 @@
 import { FaHeart, FaTrash, FaStar, FaRegStar } from "react-icons/fa";
-import img from "../assets/image.png";
+import type { FavoriteProduct } from "../Functions/Storage";
 
-export default function CardFavorito() {
+interface CardFavoritoProps {
+  product: FavoriteProduct;
+  onRemove: (id: number) => void;
+}
+
+export default function CardFavorito({ product, onRemove }: CardFavoritoProps) {
   return (
     <div className="w-[220px] bg-white rounded-2xl shadow-lg p-3 ml-[95px] mt-[25px]">
       <div className="relative bg-[#E0E0E0] rounded-xl h-[180px] flex items-center justify-center">
-        <img src={img} alt="Produto" className="h-[140px] object-contain hover:scale-150 transition"/>
+        <img src={product.image} alt="Produto" className="h-[140px] object-contain hover:scale-150 transition"/>
 
         <div className="absolute top-3 right-3 flex flex-col gap-3">
           <FaHeart className="text-red-500 text-xl cursor-pointer hover:scale-110 transition" />
-          <FaTrash className="text-gray-500 text-xl cursor-pointer hover:scale-110 transition" />
+          <FaTrash
+            onClick={() => onRemove(product.id)}
+            className="text-gray-500 text-xl cursor-pointer hover:scale-110 transition"
+          />
         </div>
       </div>
 
       <h3 className="text-sm font-medium text-[#1A1C27] mt-3">
-        Sapatos
+        {product.name}
       </h3>
 
       <div className="flex gap-1 text-indigo-500 my-1">
@@ -26,7 +34,7 @@ export default function CardFavorito() {
       </div>
 
       <p className="text-sm font-bold text-[#1A1C27]">
-        R$ 50,00 no Pix
+        {product.price} no Pix
       </p>
       <p className="text-xs text-gray-600">
         ou 2x de R$ 20,00
@@ -37,4 +45,4 @@ export default function CardFavorito() {
       </button>
     </div>
   );
-}
+}  
