c92bf896586474ad1d7c7c77e29b5831a0defe5f
Marcelo Henrique Almeida da Silva <marcelo.hasilva06@gmail.com>
2026-04-05 12:40:57 -0300

    feat: botao de continuar comprando

--- markethub-next/src/components/butoonCAr.tsx
+++ markethub-next/src/components/butoonCAr.tsx
@@ -1,8 +1,16 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 const ButoonCar = () => {
-    return(
-         <button className="w-[53%] ml-[95px] bottom-10 fixed bg-indigo-600 py-4 px-6 rounded-xl text-white font-semibold opacity-90 bg-gradient-to-r from-[#1A7FF0] to-[#8F5CFF]  hover:opacity-100 transition cursor-pointer">
-                               Continuar Comprando
-                                    </button>   
-    )
+        const router = useRouter();
+
+        return(
+                 <button
+                        onClick={() => router.push("/")}
+                        className="w-[53%] ml-[95px] bottom-10 fixed bg-indigo-600 py-4 px-6 rounded-xl text-white font-semibold opacity-90 bg-gradient-to-r from-[#1A7FF0] to-[#8F5CFF]  hover:opacity-100 transition cursor-pointer"
+                    >
+                        Continuar Comprando
+                    </button>
+        )
 }
 export default ButoonCar;
