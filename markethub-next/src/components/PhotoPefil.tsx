b9703596418295945e99d398af9e011b15a7e7f6
Ricardo <ricardonascimento1506@gmail.com>
2026-05-26 18:20:29 -0300

    ajuste photoPerfil

--- markethub-next/src/components/PhotoPefil.tsx
+++ markethub-next/src/components/PhotoPefil.tsx
@@ -19,7 +19,7 @@ const PhotoPerfil = () => {
             setDescribe(data.description ?? "");
         } else {
                 setName("");
-                setDescribe("");
+                    setDescribe("");
         }
             })
             .catch(() => {
